6141bede5d71077c07a80cf9f79b938cbd7f085f
David <david.gutierrez.dinares@juanbosco.cl>
2025-08-16 10:47:19 -0400

    Adding the array of extensions

--- browser-extensions-manager-ui/src/App.jsx
+++ browser-extensions-manager-ui/src/App.jsx
@@ -1,3 +1,28 @@
+import Extensions from "./components/Extensions";
+import data from "./data/data.json";
+import logo from "./assets/logo.svg";
+
+const extensionsArray = data.map((item) => {
+  return (
+    <Extensions
+      img={item.logo}
+      name={item.name}
+      description={item.description}
+      isActive={item.isActive}
+    />
+  );
+});
+
+console.log(extensionsArray);
+
 export default function App() {
-  return <div>Hello</div>;
+  return (
+    <>
+      <header>
+        <img src={logo} alt="The logo of the App" />
+      </header>
+
+      {extensionsArray}
+    </>
+  );
 }
---
+++ browser-extensions-manager-ui/src/components/Extensions.jsx
@@ -0,0 +1,11 @@
+export default function Extensions(props) {
+  let isItActive = props.isActive;
+
+  return (
+    <section>
+      <img src={props.img} alt="The image of the extension" />
+      <h1>{props.name}</h1>
+      <p>{props.description}</p>
+    </section>
+  );
+}
